5a9b5cd2fd8f953f293a56a69ef96e08077df04f
jwl-98 <apple_komputer@icloud.com>
2026-06-18 14:37:50 +0900

    Add Vercel Web Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
